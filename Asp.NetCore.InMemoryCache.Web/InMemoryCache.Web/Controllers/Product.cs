333195159948e7b372d07666e2969c3e32b0fa00
Burak İpekci <burakipekci@outlook.com>
2023-08-10 00:43:42 +0300

    Basic InMemoryCache

---
+++ Asp.NetCore.InMemoryCache.Web/InMemoryCache.Web/Controllers/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCache.Web.Controllers
+{
+    public class Product : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public Product(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time", DateTime.Now.ToString());//Cachelenecek verimizi ve bir key girdik.
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.zaman=_memoryCache.Get<string>("time"); //key ile alınacak datayı seçtik
+            return View();
+        }
+    }
+}
